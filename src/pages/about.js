e295b4c2a33b65fd5ecc7bdc5c2699ed1e394676
Chi Duong <chiduong4107@gmail.com>
2020-10-13 08:50:21 -0400

    Fix Responsive About Image

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,23 +15,24 @@ const AboutPage = () => {
                 <img className={aboutStyles.aboutImage} src={Pink} alt="Chi" />
                 
                 <div className={aboutStyles.aboutText}>
-                    <p><span className={aboutStyles.firstCharacter}>I</span>am a web developer living in Maryland, USA. 
-                        My name is Chi Duong (pronounced “Chee Yoong”. I know, Vietnamese people are weird. 
-                        We enunciate the "Y" sound for the letter "D"). </p>
+                    <p><span className={aboutStyles.firstCharacter}>M</span>
+                    y name is Chi Duong (pronounced “Chee Yoong”. 
+                    The "Y" sound was enunciated for the letter "D"). 
+                        I am a web developer based in Maryland, USA.</p>
 
                     <p>I was born in Vietnam, went to the USA in 2015 and started programming in Summer 2017. 
                         I earned a bachelor's degree in Computer Science in 2019. </p>
 
-                    <p>As a child, I am good at drawing and see beauty in everything. 
+                    <p>As a child, I am good at drawing and see beauty potentials in things. 
                         I am also a curious and nerdy girl. 
                         I love maths, science, technology, chess, and board games.
                         I seek to combine my artistic and problem solving skills into tech.</p>
 
-                    <p>I always wanted to make my own blog and here it is. 
+                    <p>I always wanted to make my own blog. 
                         I am looking forward to seeing how it will grow.</p>
                     
                     <p>I aspire to make intuitive, elegant, and to-the-point websites. 
-                        My desire also is to implement smart and minimalist designs into my professional and personal life.</p>
+                        My desire is to implement smart and minimalist designs into my professional and personal life.</p>
                     <p><Link to="/contact">Want to work with me? Reach out.</Link></p>
                 </div>
             </div>
