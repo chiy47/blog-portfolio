4b599cf2887d8c2f32c7cbe4abb761f1f54163c4
Chi Duong <chiduong4107@gmail.com>
2020-10-16 18:04:30 -0400

    Bold and Margin Right for Resume Location and Contact

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,21 +18,21 @@ const AboutPage = () => {
 
                 <div className={aboutStyles.aboutText}>
                     <p><span className={aboutStyles.firstCharacter}>H</span>
-                    i, my name is Chi Duong (pronounced “Chee Yoong”. 
-                    The "Y" sound is enunciated for the letter "D"). 
+                    i, my name is Chi Duong (pronounced “Chee Yoong”. In Vietnamese, 
+                    the letter "D" is pronounced like a "Y"). 
                         I am a web developer based in Maryland, USA.</p>
 
-                    <p>I was born in Vietnam, went to the USA in 2015, and started programming in Summer 2017. 
+                    <p>I was born in Vietnam, went to the USA in 2015 and started programming in Summer 2017. 
                         I earned a bachelor's degree in Computer Science in 2019.</p>
 
-                    <p>As a child, I am good at drawing, very excited to see beauty potentials in thing,
-                        and a nerdy girl enjoying maths, science, and technology. 
+                    <p>As a child, I was good at drawing. I am very excited to see the potential beauty in everything. 
+                        Other interests include math, science, and technology. 
                         As a result, I seek to implement my artistic and problem-solving skills into tech.</p>
 
                     <p>I always wanted to make my own blog. 
                         I am looking forward to seeing its growth.</p>
 
-                    <p>In my free time, I like photography, drawing, playing board games, etc.</p>
+                    <p>In my free time, I like photography, drawing and playing board games.</p>
 
                     <p><Link to="/contact" className={aboutStyles.workWithMe}>Want to work with me? Reach out.</Link></p>
                 </div>
